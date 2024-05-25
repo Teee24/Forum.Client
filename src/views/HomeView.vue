be36909512648b5300c97931543fd38a20ef829e
tina <tinalien0728@gmail.com>
2024-05-26 00:25:54 +0800

    feat : get的BS模板

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,5 +32,35 @@ onMounted(async()=>{
     <a class="nav-link " >娛樂</a>
   </li>
 </ul>
+<div class="list-group list-group-flush">
+  <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+    <div class="d-flex w-100 justify-content-between">
+      <p class="mb-1">Publisher</p>
+      <small>Date</small>
+    </div>
+    <h5 class="mb-1">Title</h5>
+    <small>Detail...</small>
+    <br>
+    <div><h5 class="badge rounded-pill bg-secondary">Category</h5></div>
+    
+  </a>
+  <hr>
+  <a href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Some placeholder content in a paragraph.</p>
+    <small class="text-muted">And some muted small print.</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Some placeholder content in a paragraph.</p>
+    <small class="text-muted">And some muted small print.</small>
+  </a>
+</div>
 </div>
 </template>
